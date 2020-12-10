301917d8fae5c098f36a0d8f9d71d16e0bb6b51f
Ruslan Golovizin <r.golovizin@original-group.ru>
2020-12-10 21:37:56 +0300

    Added error handler to paint calculator

--- colorcalc/colorcalc.go
+++ colorcalc/colorcalc.go
@@ -1,21 +1,50 @@
 package main
 
-import "fmt"
-
-var metersPerLiter float64 = 0
+import (
+	"fmt"
+	"log"
+)
 
 func main() {
-	var total float64
+	var total, amount float64
+	var err error
 
-	total += paintNeeded(4.2, -3.0)
-	total += paintNeeded(5.3, 4.4)
-	total += paintNeeded(2.1, 3.7)
+	amount, err = paintNeeded(4.2, 3.0)
 
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	total += amount
+	amount, err = paintNeeded(5.3, -4.4)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	total += amount
+	amount, err = paintNeeded(2.1, 3.7)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	total += amount
 	fmt.Printf("Total: %0.2f liters\n", total)
 }
 
-func paintNeeded(width float64, height float64) float64 {
+func paintNeeded(width float64, height float64) (float64, error) {
+	var metersPerLiter float64 = 10
+
+	if width < 0 {
+		return 0, fmt.Errorf("a with of %0.2f is invalid", width)
+	}
+
+	if height < 0 {
+		return 0, fmt.Errorf("a height of %0.2f is invalid", height)
+	}
+
 	area := width * height
 
-	return area / metersPerLiter
+	return area / metersPerLiter, nil
 }
