3cbc337e8495be388afa40a06bd4245e59a8beaa
Ruslan Golovizin <r.golovizin@original-group.ru>
2020-12-12 17:26:17 +0300

    Added program for read file

--- src/average/main.go
+++ src/average/main.go
@@ -1,9 +1,14 @@
 package main
 
-import "fmt"
+import (
+	"filereader"
+	"fmt"
+)
 
 func main() {
 	numbers := [3]float64{71.8, 56.2, 89.5}
+	filereader.Read()
+
 	var sum float64
 
 	for _, value := range numbers {
