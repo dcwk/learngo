a6a820ff2290b8c9482e17c41b15992c198135af
dcwk <dcwk@yandex.ru>
2022-04-17 19:27:41 +0300

    Added formatted hello endpoint

--- src/webapp/main.go
+++ src/webapp/main.go
@@ -13,8 +13,18 @@ func viewHandler(writer http.ResponseWriter, request *http.Request) {
 	}
 }
 
+func formattedViewHandler(writer http.ResponseWriter, request *http.Request) {
+	message := []byte("<h1>Hello web!</h1>")
+	_, err := writer.Write(message)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
 func main() {
 	http.HandleFunc("/hello", viewHandler)
+	http.HandleFunc("/formatted-hello", formattedViewHandler)
+
 	err := http.ListenAndServe("localhost:8081", nil)
 	log.Fatal(err)
 }
